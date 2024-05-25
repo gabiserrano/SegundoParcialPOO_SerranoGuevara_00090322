b649f838879b02c7debe7df383054522e90bfa8e
gabiserrano <104926775+gabiserrano@users.noreply.github.com>
2024-05-25 13:17:51 -0600

    Empresa Parcial

--- SegundoParcialPOO_SerranoGuevara_00090322/src/Departamento.java
+++ SegundoParcialPOO_SerranoGuevara_00090322/src/Departamento.java
@@ -1,45 +1,41 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Departamento implements ComponentOrganizacional{
+public class Departamento implements ComponentOrganizacional {
     private String nombre;
     private List<ComponentOrganizacional> componentes;
 
-    public Departamento(String desarrolloDeSoftware) {
-    }
-
-    public void Departamento (String nombre){
+    // Proper constructor that initializes the 'nombre' and 'componentes' list
+    public Departamento(String nombre) {
         this.nombre = nombre;
         this.componentes = new ArrayList<>();
-
     }
 
-    public void agregarComponentes(ComponentOrganizacional componente){
-        componentes.add (componente);
+    public void agregarComponentes(ComponentOrganizacional componente) {
+        componentes.add(componente);
     }
 
-    public void removerComponente (ComponentOrganizacional componente){
-    componentes.remove(componente);
+    public void removerComponente(ComponentOrganizacional componente) {
+        componentes.remove(componente);
     }
 
-    //@Override
-    public void mostrarEstructura(){
+    @Override
+    public void mostrarEstructura() {
         System.out.println("Departamento: " + nombre);
-        for (ComponentOrganizacional componente : componentes){
+        for (ComponentOrganizacional componente : componentes) {
             componente.mostrarEstructura();
         }
     }
 
-    public String getNombre(){
+    public String getNombre() {
         return nombre;
     }
 
-    public void getNombre(String nombre){
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    public List<ComponentOrganizacional> getComponentes(){
+    public List<ComponentOrganizacional> getComponentes() {
         return componentes;
     }
-
 }
